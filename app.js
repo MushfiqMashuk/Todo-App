6ddd9ee5cf0a04031a86dd62a3c6d73a09297eaf
Mushfiq Mashuk <mushfiqmashuk96@gmail.com>
2021-08-18 02:10:36 +0600

    initial commit

--- app.js
+++ app.js
@@ -9,25 +9,14 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input[type="text"]');
 const addedTaskList = document.querySelector('#added-task-list');
+const completedTaskList = document.querySelector('#completed-task-list');
 
 const addTask = function(event) {
     event.preventDefault();
 
     if(input.value && input.value !== ""){
-        const listItem = document.createElement('li');
-        const checkBox = document.createElement('input');
-        const label = document.createElement('label');
-
-        checkBox.type = 'checkbox';
-        label.innerText = input.value;
-
-        listItem.appendChild(checkBox);
-        listItem.appendChild(label);
-
-        addedTaskList.appendChild(listItem);
-        input.value = "";
-
-        bindIncompleteTask(checkBox);
+        
+        createTask(input.value);
 
     } else {
         alert("Please something to your To-Do");
@@ -35,9 +24,45 @@ const addTask = function(event) {
 
 }
 
-const bindIncompleteTask = function(checkBox) {
-    checkBox.addEventListener('change', )
+const createTask = function (task) {
+    const listItem = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const label = document.createElement('label');
+
+    checkBox.type = 'checkbox';
+    label.innerText = task;
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    addedTaskList.appendChild(listItem);
+    input.value = "";
+
+    checkBox.onchange = completeTask;
+};
+
+const completeTask = function (event) {
+    const checkBox = event.target;
+    const listItem = checkBox.parentNode;
+    const button = document.createElement('button');
+
+    button.innerText = 'Delete';
+    button.className = 'delete';
+
+    listItem.appendChild(button);
+    checkBox.remove();
+
+    button.onclick = deleteTask;
+
+    completedTaskList.appendChild(listItem);
+
 }
+
+const deleteTask = function (event) {
+    const listItem = event.target.parentNode;
+
+    completedTaskList.removeChild(listItem);
+};
 
 
 form.addEventListener('submit', addTask);
